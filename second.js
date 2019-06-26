0db90dd01a582fd27576322c8961b174c60e2e7c
sakigo9 <saketprag322@gmail.com>
2019-06-27 00:03:19 +0530

    tic toc game

---
+++ second.js
@@ -0,0 +1,21 @@
+console.log("connected")
+var restart = document.querySelector("#b");
+var squares = document.querySelectorAll('td');
+function clear(){
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent='';
+  }
+}
+restart.addEventListener('click',clear)
+function clean(){
+  if(this.textContent ===''){
+    this.textContent='x';
+  }else if (this.textContent ==='x') {
+    this.textContent='o';
+  }else {
+    this.textContent='';
+  }
+}
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click',clean)
+}
